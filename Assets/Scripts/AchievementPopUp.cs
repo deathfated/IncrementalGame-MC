befee49fd96f0d1aedbca7cf6e369e683be2aa65
deathfated <60390126+deathfated@users.noreply.github.com>
2022-08-03 17:09:06 +0700

    Finished!
    now to build...

---
+++ Assets/Scripts/AchievementPopUp.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+
+public class AchievementPopUp : MonoBehaviour
+
+{
+    private static AchievementPopUp _instance = null;
+
+    public static AchievementPopUp Instance
+
+    {
+
+        get
+
+        {
+
+            if (_instance == null)
+
+            {
+
+                _instance = FindObjectOfType<AchievementPopUp>();
+
+            }
+
+
+
+            return _instance;
+
+        }
+
+    }
+
+
+
+    [SerializeField] private Transform _popUpTransform;
+
+    [SerializeField] private Text _popUpText;
+
+    [SerializeField] private float _popUpShowDuration = 3f;
+
+    [SerializeField] private List<AchievementData> _achievementList;
+
+
+
+    private float _popUpShowDurationCounter;
+
+
+
+    private void Update()
+
+    {
+
+        if (_popUpShowDurationCounter > 0)
+
+        {
+
+
+            _popUpShowDurationCounter -= Time.unscaledDeltaTime;
+
+            // Lerp adalah fungsi linear interpolation, digunakan untuk mengubah value secara perlahan
+
+            _popUpTransform.localScale = Vector3.LerpUnclamped(_popUpTransform.localScale, Vector3.one, 0.5f);
+
+        }
+
+        else
+
+        {
+
+            _popUpTransform.localScale = Vector2.LerpUnclamped(_popUpTransform.localScale, Vector3.right, 0.5f);
+
+        }
+
+    }
+
+
+
+    public void UnlockAchievement(AchievementType type, string value)
+
+    {
+        AchievementData achievement = _achievementList.Find(a => a.Type == type && a.Value == value);
+
+        if (achievement != null && !achievement.IsUnlocked)
+
+        {
+
+            achievement.IsUnlocked = true;
+
+            ShowAchivementPopUp(achievement);
+
+        }
+
+    }
+
+
+
+    private void ShowAchivementPopUp(AchievementData achievement)
+
+    {
+
+        _popUpText.text = achievement.Title;
+
+        _popUpShowDurationCounter = _popUpShowDuration;
+
+        _popUpTransform.localScale = Vector2.right;
+
+    }
+
+}
+
+[System.Serializable]
+
+public class AchievementData
+
+{
+
+    public string Title;
+
+    public AchievementType Type;
+
+    public string Value;
+
+    public bool IsUnlocked;
+
+}
+
+
+
+public enum AchievementType
+
+{
+
+    UnlockResource
+
+}
